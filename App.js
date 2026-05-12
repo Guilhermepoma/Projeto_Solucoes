1cde1ef7c61dfefd1fbbece83043a52d6e1c102d
Guilherme_Poma_Cercena <guilhermepomacercena@gmail.com>
2026-05-12 14:36:33 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking } from 'react-native';
 
 export default function App() {
+  const abrirMapa = async () => {
+    const url = 'https://www.google.com/maps?q=-28.6775,-49.3697';
+
+    const supported = await Linking.canOpenURL(url);
+
+    if (supported) {
+      await Linking.openURL(url);
+    } else {
+      console.log('Não foi possível abrir o mapa');
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button title="Abrir Google Maps" onPress={abrirMapa} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
