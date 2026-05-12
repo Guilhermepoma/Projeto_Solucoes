3d68607e928154561a9b121cb4ccf16a2c09d38b
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2026-05-12 15:13:27 -0300

    a

--- App.js
+++ App.js
@@ -1,19 +1,114 @@
-import { Button, Linking } from 'react-native';
+// App.js
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
-  const abrirMapa = async () => {
-    const url = 'https://www.google.com/maps?q=-28.6775,-49.3697';
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      await Linking.openURL(url);
-    } else {
-      console.log('Não foi possível abrir o mapa');
+  const fazerLogin = () => {
+    if (!email || !senha) {
+      Alert.alert("Erro", "Preencha todos os campos");
+      return;
     }
+
+    Alert.alert("Login realizado", `Bem-vindo ${email}`);
   };
 
   return (
-    <Button title="Abrir Google Maps" onPress={abrirMapa} />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.card}>
+        <Text style={styles.titulo}>Login</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          secureTextEntry
+          value={senha}
+          onChangeText={setSenha}
+        />
+
+        <TouchableOpacity style={styles.botao} onPress={fazerLogin}>
+          <Text style={styles.textoBotao}>Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.link}>Criar conta</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0F172A",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+
+  card: {
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    padding: 25,
+    elevation: 5,
+  },
+
+  titulo: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginBottom: 25,
+    textAlign: "center",
+    color: "#0F172A",
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#CBD5E1",
+    borderRadius: 12,
+    padding: 15,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+
+  botao: {
+    backgroundColor: "#2563EB",
+    padding: 16,
+    borderRadius: 12,
+    alignItems: "center",
+    marginTop: 10,
+  },
+
+  textoBotao: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  link: {
+    marginTop: 20,
+    textAlign: "center",
+    color: "#2563EB",
+    fontSize: 16,
+  },
+});
